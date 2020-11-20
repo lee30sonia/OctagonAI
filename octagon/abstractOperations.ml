abd2ce02a273bbaeaf4866219be89ab1473bb5eb
Sonia <lee30sonia@gmail.com>
2020-11-20 14:22:45 +0100

    basic data types and operations

---
+++ octagon/abstractOperations.ml
@@ -0,0 +1,9 @@
+(* Define basic operations in the abstract domain *)
+open Types
+
+(* The abstraction function maps from a program state to a DBM. *)
+let alpha state : DBM =
+
+let join a1 a2 : DBM =
+
+let meet a1 a2 : DBM =
