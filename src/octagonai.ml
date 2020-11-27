aa058520b920d9d8ef97a202a66effe82fc4957a
Orégane <oregane.desrentes@ens-lyon.fr>
2020-11-27 17:07:06 +0100

    first exploration

--- src/octagonai.ml
+++ src/octagonai.ml
@@ -1,1 +1,118 @@
 (* main file, gets the test file (maybe interactive input later), and divides the work *)
+(*open Frontc;;*)
+open List;;
+open Cabs;;
+
+(*type my_name = string * string list*)
+
+type typee = string
+type str_name = string
+type arg_list = (typee * str_name) list
+type decl = typee * str_name * arg_list option
+type signature = typee * arg_list option
+
+
+let rec parse_base_type (base_type : base_type) : signature  = 
+    match base_type with
+	| PROTO (base_typetwo, singlenamelist, threedots) ->
+        let (tpe, args) = parse_base_type base_typetwo in
+        let new_args = map (fun e -> let typee, name, arg_list = parse_single_name e in typee, name) singlenamelist in
+        (tpe, match args with None -> Some new_args | Some last_args -> Some (last_args @ new_args))
+	| INT (_, _) -> ("int", None)
+    | PTR (base_typetwo) ->
+        let (tpe, args) = parse_base_type base_typetwo in
+        ("("^tpe^"*)", args)
+    | _ -> failwith "not implemented"
+
+and parse_name (name : name) : decl =
+    let string_name, base_type, gnu_attrs, expression = name in
+    match parse_base_type base_type with
+    | (tpe, args) -> (tpe, string_name, args)
+
+(*and parse_single_name (sname : single_name) : string * string list = parse_single_name true sname*)
+
+and parse_single_name (sname : single_name) : decl =
+    let base_type, storage, name = sname in
+    match parse_base_type base_type with
+    | (_, Some _) -> failwith "Impossible case: functions cannot return functions"
+    | (tpe, None) -> parse_name name
+    
+
+(*
+let extract_type_name a =
+    let a_type, a_storage, a_gnuattr = a in
+    let str_name, type_again, a_list, something =  a_gnuattr  in
+    (str_name, type_again)
+in
+
+let rec analyseCvariables parsed_function variable_list = match parsed_function with
+    | Cabs.DECDEF(type_name, storage, name_list) :: t ->  analyseCvariables t ((map extract_type_name name_list) @ variable_list)
+    | _ -> variable_list
+in
+
+let rec analyseC parsed_function argument_list = 
+    let variable_list = analyseCvariables parsed_function argument_list in variable_list
+in
+
+let rec analyseCblock parsed_list = match parsed_list with
+    | [] -> []
+    | (Cabs.FUNDEF (f,body))::t -> begin
+            let (fun_type, fun_storage, fun_name) = f in
+            let (str_name, fun_proto, fun_gnuattrs, fun_something) = fun_name in
+            let Cabs.PROTO(another_type, fun_args, fun_somethingbool) = fun_proto in
+            
+            let definition, statement = body in
+
+            (str_name, analyseC definition (map extract_type_name fun_args)) :: analyseCblock t
+            end
+    | _ -> print_string("other"); []
+in 
+*)
+
+let rec analyseCblock parsed_list : decl list = match parsed_list with
+    | [] -> []
+    | (Cabs.FUNDEF (f,body))::t -> begin
+            (parse_single_name f) :: analyseCblock t
+            end
+    | _ -> print_string("other"); []
+
+let rec print_args (l: arg_list) : unit =
+    match l with
+    | (tpe, name)::t ->
+        print_endline ("\t" ^ tpe ^ " " ^ name);
+        print_args t
+    | [] -> ()
+
+let rec print_decl_list (l: decl list) : unit =
+    match l with
+    | (tpe, name, args)::t ->
+        print_endline (tpe ^ " " ^ name);
+        begin match args with
+        | None -> ()
+        | Some args_l -> print_args args_l
+        end;
+        print_decl_list t
+    | [] -> ()
+
+
+let start_parse () = 
+    match Frontc.parse_file "./test.c" stdout with
+        | Frontc.PARSING_ERROR -> print_string "error"; []
+        | Frontc.PARSING_OK(l) -> l
+
+
+let _ =
+    let parsed = start_parse () in
+    analyseCblock parsed |> print_decl_list;
+    parsed
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.c
@@ -0,0 +1,23 @@
+int coucou(int c, int d) {}
+int main() {}
+
+int (*a(int c))(int b) {}
+
+int *b() {}
+
+
+/*
+int coucou(int c) {
+    int b = 9;
+    int a = 2;
+    return 3;
+}
+
+int main() {
+    int a = 0;
+    int i;
+    for (i = 0; i<10; i++) {
+        a = a+i;
+    }
+    return a;
+}*/
